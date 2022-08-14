98c1670e171ef2d6f19f5ef66c499ec68d8c1248
hktrollgames <hktrollgames@gmail.com>
2022-08-14 13:45:47 +0900

    JsonUtility 사용

--- Assets/DataManager.cs
+++ Assets/DataManager.cs
@@ -2,9 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json;
+using System.IO;
 
 public class DataManager : MonoBehaviour
 {
+    [ContextMenu("ReadJson")]
+    void ReadJson()
+    {
+        var textAsset = Resources.Load("JsonData/MyClass") as TextAsset;
+        myClasses = JsonUtility.FromJson<List<MyClass>>(textAsset.text);
+        //myClasses = JsonConvert.DeserializeObject<List<MyClass>>(textAsset.text);
+        print(myClasses[0].name);
+    }
+
+    public string myString = "default111";
+    [ContextMenu("SaveJson")]
+    void SaveJson()
+    {
+        var jsonString = JsonUtility.ToJson(this);
+        string savePath = Application.dataPath + "/myFile.txt";
+        print(savePath);
+        File.WriteAllText(savePath, jsonString);
+        UnityEditor.AssetDatabase.Refresh();
+    }
+
     [ContextMenu("ReadExcel")]
 
     void ReadExcel()
