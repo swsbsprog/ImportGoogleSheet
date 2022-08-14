2ecb09e284f49e7cc8891bec9dde47b16ad39a66
hktrollgames <hktrollgames@gmail.com>
2022-08-14 13:35:34 +0900

    초기 커밋.

---
+++ Assets/DataManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+
+public class DataManager : MonoBehaviour
+{
+    [ContextMenu("ReadExcel")]
+
+    void ReadExcel()
+    {
+        var textAsset = Resources.Load("JsonData/MyClass") as TextAsset ;
+        myClasses = JsonConvert.DeserializeObject<List<MyClass>>(textAsset.text);
+        print(myClasses[0].name);
+    }
+    public List<MyClass> myClasses = new List<MyClass>();
+}
+
+[System.Serializable]
+public class MyClass
+{
+    public int id;
+    public string name;
+    public int price;
+}
